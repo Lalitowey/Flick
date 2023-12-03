b1f3fd179c1a09ae7176c546e770aea1035a7c30
Lalitowey <91431946+Lalitowey@users.noreply.github.com>
2023-12-02 16:53:54 -0800

    Steven's swiper update + Eduardo's media query

--- script.js
+++ script.js
@@ -1,26 +1,66 @@
-// JavaScript source code
-//
-var form = document.getElementById('checkingPasswords');
-var new_password = form.new_password;
-var confirm_new_password = form.confirm_new_password;
-var alertText = document.getElementById('alert');
-let confirmed = false;
+// DOM
+const swiper = document.querySelector('#swiper');
+const like = document.querySelector('#like');
+const dislike = document.querySelector('#dislike');
 
-confirm_new_password.addEventListener('input', () => {
-    if (new_password.value != confirm_new_password.value) {
-        alertText.innerHTML = 'Passwords do not match.';
-        confirmed = false;
-    }
-    else {
-        alertText.innerHTML = '';
-        confirmed = true;
-    }
-});
-form.onsubmit = function () {
-    if (confirmed) {
-        alert('Password has been updated!');
-        return true;
-    }
-    alert('Try again.');
-    return false;
-};
+
+// constants
+const apiUrl = 'https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc';
+const apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MTRhZjQ4YTRjZjlmYThkODA2OTA0ODM0ZDlhMDYyYiIsInN1YiI6IjY1MTRiYTI4OTNiZDY5MDEzOGZiZjQxZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.E_AHqlFdOwGdJHZhcYHEFqsASC8XlhkxXAVvkrplQYI'; // Replace with your actual API key
+const urls = [];
+
+
+//variables
+let cardCount = 0;
+
+//functions
+function fetchImages() {
+  fetch(apiUrl, {
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+    },
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Extract image URLs from the API response
+      const results = data.results;
+      if (results && results.length > 0) {
+        const imageUrls = results.map(result => `https://image.tmdb.org/t/p/w500/${result.poster_path}`);
+        // Update the urls array with the new image URLs
+        urls.splice(0, urls.length, ...imageUrls);
+
+        // Append the new cards with the fetched image URLs
+        for (let i = 0; i < 10; i++) {
+          appendNewCard();
+        }
+      } else {
+        console.error('No results found in the API response.');
+      }
+    })
+    .catch(error => console.error('Error fetching images:', error));
+}
+
+function appendNewCard() {
+  const card = new Card({
+    imageUrl: urls[cardCount % 10],
+    onDismiss: appendNewCard,
+    onLike: () => {
+      like.style.animationPlayState = 'running';
+      like.classList.toggle('trigger');
+    },
+    onDislike: () => {
+      dislike.style.animationPlayState = 'running';
+      dislike.classList.toggle('trigger');
+    },
+  });
+  swiper.append(card.element);
+  cardCount++;
+
+  const cards = swiper.querySelectorAll('.card:not(.dismissing)');
+  cards.forEach((card, index) => {
+    card.style.setProperty('--i', index);
+  });
+}
+
+// Initial fetch and card creation
+fetchImages();
